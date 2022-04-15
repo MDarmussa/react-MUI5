ab2015c6470320d96b411817cf72cc4a8f3f8d8e
Mohammad <mhamed.mussa@gmail.com>
2022-04-15 03:06:29 -0500

    first commit

---
+++ src/pages/Tour.js
@@ -0,0 +1,34 @@
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import Box from "@mui/system/Box";
+import ImageCollage from '../components/ImageCollage'
+import Accordian from '../components/Accordian'
+
+const Tour = () => 
+<Container sx={{ width: 900 }}>
+     <Typography variant="h3" component='h1' marginTop={3}>
+          Explore the World in Vegas
+     </Typography>
+     <Box marginTop={3} sx={{dispaly: 'flex'}}>
+          <img 
+               src="https://media.istockphoto.com/photos/welcome-to-fabulous-las-vegas-sign-picture-id531128595?s=612x612" alt="" height={325} 
+          />
+          <ImageCollage />
+     </Box>
+     <Box>
+          <Typography variant="h6" component='h4' marginTop={3}>
+               About this Ticket
+          </Typography>
+          <Typography variant="paragraph" component='p' marginTop={3}>
+               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </Typography>
+     </Box>
+     <Box>
+          <Typography variant="h6" component='h4' marginTop={3}>
+                Frequently Asked Questions
+          </Typography>
+          <Accordian />
+     </Box>
+</Container>
+
+export default Tour;
