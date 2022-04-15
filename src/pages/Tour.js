5ea7899804f11d1e84a1262dd779f390c69956ed
Mohammad <mhamed.mussa@gmail.com>
2022-04-15 13:20:35 -0500

    added calender for booking

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,8 +1,16 @@
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Box from "@mui/system/Box";
 import ImageCollage from '../components/ImageCollage'
 import Accordian from '../components/Accordian'
+import CustomizedAccordions from "../components/Accordian";
+import React from "react";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import StaticDateRangePickerDemo from "../components/DatePicket";
+
+
+
 
 const Tour = () => 
 <Container sx={{ width: 900 }}>
@@ -23,12 +31,15 @@ const Tour = () =>
                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </Typography>
      </Box>
-     <Box>
-          <Typography variant="h6" component='h4' marginTop={3}>
+     <Box marginBottom={10}>
+          <Typography variant="h6" component='h4' marginTop={3} marginBottom={2}>
                 Frequently Asked Questions
           </Typography>
-          <Accordian />
+          <CustomizedAccordions />
      </Box>
+     <BottomNavigation>
+          <StaticDateRangePickerDemo />
+     </BottomNavigation>
 </Container>
 
 export default Tour;
